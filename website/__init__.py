32846fecf49cfff55c64032830b1e7c26568058b
Julie Luu <julie.luu.jl@googlemail.com>
2021-08-21 23:09:00 +0100

    refactoring code

--- website/__init__.py
+++ website/__init__.py
@@ -1,13 +1,17 @@
 # Setting up Flask Application
-from flask import Flask
-from flask_session import Session
-from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_session import Session
 
-# Defining the database
 db = SQLAlchemy()
-DB_NAME = "database.db"
+db_name = "database.db"
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.login_message_category = 'info'
 
 def create_app():
   # represents the name of the file
@@ -16,8 +20,13 @@ def create_app():
   app.config['SECRET_KEY'] = '1533e2ab57a4a84099e9ba68935b9d44'
   app.config["SESSION_TYPE"] = "filesystem"
   # links database to app
-  app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+  app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_name}'
+  # initialising the db
   db.init_app(app)
+  # initialising the bycrpt
+  bcrypt.init_app(app)
+  # initialising the login_manager
+  login_manager.init_app(app)
   Session(app)
   
   # registering our Blueprint
@@ -28,24 +37,15 @@ def create_app():
   app.register_blueprint(views, url_prefix='/')
   app.register_blueprint(auth, url_prefix='/')
   
-  # db classes
-  from .models import User, Note
+  # db class
+  from website.models import User
 
   create_database(app)
-
-  # authorises login
-  login_manager = LoginManager()
-  login_manager.login_view = 'auth.login'
-  login_manager.init_app(app)
-
-  @login_manager.user_loader
-  def load_user(id):
-    return User.query.get(int(id))
 
   return app
 
 # if database does not exist, create database
 def create_database(app):
-  if not path.exists('website/' + DB_NAME):
+  if not path.exists('website/' + db_name):
       db.create_all(app=app)
-      print('Created Database!')
+      print('Database created')
