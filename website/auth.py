5bcf5056777fdc4d9d1fc8300112c071acef7eab
Julie Luu <julie.luu.jl@googlemail.com>
2021-08-21 23:12:37 +0100

    refactoring code
    removing some modules that are not needed
    updating login validation process
    updating the registration validation process
    updating logout route

--- website/auth.py
+++ website/auth.py
@@ -1,66 +1,59 @@
-# authorisation
-
-from website.forms import RegistrationForm, LoginForm
+# authorisation routes
 from flask import Blueprint, render_template, url_for, flash, redirect, request
-from .views import views
-from .models import User
-from werkzeug.security import generate_password_hash, check_password_hash
-from . import db
 from flask_login import login_user, login_required, logout_user, current_user
+from website import db, bcrypt
+from website.models import User
+from website.forms import RegistrationForm, LoginForm
 
 # defining this file is a Blueprint of our application, it has a group of URL's
 
 auth = Blueprint('auth', __name__)
 
+# login route
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+  # checks if the user is logged in, if it is direct to the homepage
+  if current_user.is_authenticated:
+    return redirect(url_for('views.home'))
   login_form = LoginForm()
-  if request.method == 'POST':
-    email = request.form.get('email')
-    password = request.form.get('password')
-        
+  if login_form.validate_on_submit():
     # checks to see if the user is in the database
-    user = User.query.filter_by(email=email).first()
-
-    # checks password entered matches the stored hashed password 
+    user = User.query.filter_by(email=login_form.email.data).first()
     if user:
-      if check_password_hash(user.password, password):
-        flash('Logged in successfully!', category='success')
-        login_user(user, remember=True)
+      # checks password entered matches the stored hashed password 
+      if bcrypt.check_password_hash(user.password, login_form.password.data):
+        login_user(user, remember=login_form.remember.data)
         return redirect(url_for('views.home'))
       else:
-        flash('Incorrect password, try again.', category='error')
-    else:
-      flash('Email does not exist.', category='error')
+        flash('Incorrect password. Please try again!', 'danger')
+    else: 
+      flash('User account does not exist. Please try again!', 'danger')
+  return render_template('login.html', title='Login', form=login_form)
 
-  return render_template("login.html", title='Login', form=login_form, boolean=True)
-
+# registration route
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
-  # creates an instance
+  # checks if the user is logged in, if it is direct to the homepage
+  if current_user.is_authenticated:
+    return redirect(url_for('views.home'))
+  # creates an instance for the RegistrationForm class
   register_form = RegistrationForm()
-  
   if register_form.validate_on_submit():
-    if request.method == 'POST':
-      # gets email address from form to run query
-      email = request.form.get('email_address')
-
-      # checks to see if the user is in the database
-      user = User.query.filter_by(email=email).first()
-
-      if user:
-        flash('Email address already exists.', category='error')
-      else:
-        if register_form.validate_on_submit():
-          flash(f'Account created for {register_form.username.data}!', 'success')
-          # gets new user information from registration form and stores it in the database
-          new_user = User(email=request.form.get('email_address'), first_name=request.form.get('first_name'), password=generate_password_hash(
-          request.form.get('password'), method='sha256')) # hashes the password before it is stored for security
-          db.session.add(new_user)
-          db.session.commit()
-          return redirect(url_for('auth.register'))
+    # hashing the password to a string
+    hashed_password = bcrypt.generate_password_hash(register_form.password.data).decode('utf-8')
+    
+    # creating new instance of user
+    user = User(first_name=register_form.first_name.data, last_name=register_form.last_name.data, username=register_form.username.data, email=register_form.email.data, password=hashed_password)
+    db.session.add(user)
+    db.session.commit()
+    
+    # if successful display message and redirect user to login page
+    flash(f'Your account has been created. You can now login!', 'success')
+    return redirect(url_for('auth.login'))
   return render_template("sign_up.html", title='Register', form=register_form)
 
+# logout route
 @auth.route('/logout')
 def logout():
-  return '<p>Logout</p>'
+  logout_user()
+  return redirect(url_for('views.home'))
