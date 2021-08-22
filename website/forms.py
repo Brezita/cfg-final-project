36c3628489e1020f0ebe9e054118945c0f664b24
Julie Luu <julie.luu.jl@googlemail.com>
2021-08-22 11:48:01 +0100

    refactoring code

--- website/forms.py
+++ website/forms.py
@@ -10,10 +10,8 @@ class RegistrationForm(FlaskForm):
   first_name = StringField("First Name", validators=[DataRequired()])
   last_name = StringField("Last Name", validators=[DataRequired()])
   email = StringField('Email Address', validators=[DataRequired(), Email()])
-  username = StringField('Username', 
-  validators=[DataRequired(), Length(min=5, max=20)])
-  password = PasswordField('Password', 
-  validators=[DataRequired(), Length(min=8, max=20)])
+  username = StringField('Username', validators=[DataRequired(), Length(min=5, max=20)])
+  password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=20)])
   confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), Length(min=8, max=20), EqualTo('password', message='Passwords must match')])
   submit = SubmitField("Create Account")
   
@@ -32,7 +30,6 @@ class RegistrationForm(FlaskForm):
 # Login Form Class
 class LoginForm(FlaskForm):
   email = StringField('Email Address', validators=[DataRequired(), Email()])
-  password = PasswordField('Password', 
-  validators=[DataRequired()])
+  password = PasswordField('Password', validators=[DataRequired()])
   remember = BooleanField('Remember Me')
   submit = SubmitField("Login")
